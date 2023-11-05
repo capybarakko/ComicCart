dd363e417a25dc7bb85ff200dab91b068150f880
KengoKashihara <KengoKashihara1998@gmail.com>
2023-11-05 23:41:52 +0900

    ベルアラートからデータを取得

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
 function main(){
     console.log("test");
+
+    var loginData = {
+        'email':"KengoKashihara1998@gmail.com",
+        'password':"Rakko3150K61"
+    };
+
+    var loginResponse = UrlFetchApp.fetch("https://alert.shop-bell.com/users/login/",loginData);
+    var calenderResponse = UrlFetchApp.fetch("https://alert.shop-bell.com/calendar/288953/202311/");
+    var content = calenderResponse.getContentText();
+    var table = Parser.data(content).from('<table id="reflow" class="table">').to('</table>').iterate();
+
+    console.log(table);
 }
